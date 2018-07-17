2afdb19a0d356cfc8fa69f40ce812e66ad898dd2
Mikhail Petrushkov <petrushkov.mikhail@gmail.com>
2018-07-17 11:16:48 +0700

    Update version

--- fixbin.py
+++ fixbin.py
@@ -10,15 +10,16 @@ if len(sys.argv) > 1:
     filename = sys.argv[1]
 
 shortname = basename(filename).replace(".bin", "")
+innername = "coronaHtml5App"
 
 with zipfile.ZipFile(filename) as temp:
-    with temp.open('{0}.js'.format(shortname)) as myfile:
+    with temp.open('{0}.js'.format(innername)) as myfile:
         newfile = myfile.read().decode('UTF-8')
         newfile = newfile.replace('window.innerHeight', 'getActualHeight()').replace('window.innerWidth', 'getActualWidth()')
 
     with zipfile.ZipFile('{0}.new'.format(filename), 'w', compression=zipfile.ZIP_DEFLATED) as zipFile:
-        zipFile.writestr('{0}.js'.format(shortname), newfile)
-        zipFile.writestr('{0}.html.mem'.format(shortname), temp.open('{0}.html.mem'.format(shortname)).read())
+        zipFile.writestr('{0}.js'.format(innername), newfile)
+        zipFile.writestr('{0}.html.mem'.format(innername), temp.open('{0}.html.mem'.format(innername)).read())
 
 bakFile = '{0}.bak'.format(filename)
 counter = 1
